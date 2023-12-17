e7ebfb7bb7ae1988c3f999fcaf289a7b0c77a8ce
Shantanu Mahale <46351765+shantanubr@users.noreply.github.com>
2023-12-17 20:50:44 +0530

    update readme

--- hooks/usePlayer.tsx
+++ hooks/usePlayer.tsx
@@ -1,7 +1,9 @@
+import { cloneDeep } from "lodash";
 import { useState } from "react";
 
 const usePlayer = () => {
   const [players, setPlayers] = useState({});
+  const newPlayer = cloneDeep(players);
   return {
     players,
     setPlayers,
